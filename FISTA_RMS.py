ebcede230e69c4c12c729fbc2967367d14eeb05e
miguelcarcamov <miguel.carcamo@protonmail.com>
2019-03-21 16:09:46 +0000

    added RMSE in python notebooks

--- FISTA_RMS.py
+++ FISTA_RMS.py
@@ -113,3 +113,42 @@ def FISTA_Mix(P, W, K, phi, lambda2, lambda2_ref, m, n, soft_threshold, iteratio
             
             F_thick = real_Xthick + 1j* imag_Xthick
     return F_thin+F_thick
+
+
+def FISTA_Mix2(P, W, K, phi, lambda2, lambda2_ref, m, n, soft_threshold, iterations):
+    F_thin = form_F_dirty(K, P, phi, lambda2, lambda2_ref, n)
+    F_thick = np.zeros(n) + 1j*np.zeros(n)
+    db8 = pywt.Wavelet('db8')
+    for i in range(0, iterations):
+            #Thin structures
+            F_comb = form_P_meas(W, F_thin+F_thick, phi, lambda2, m)
+            residual = P - F_comb
+            F_thin = F_thin + form_F_li(residual, phi, lambda2, lambda2_ref, n)
+            Xreal = F_thin.real
+            Ximag = F_thin.imag
+            X_tempreal = softThreshold(Xreal, soft_threshold)
+            X_tempimag = softThreshold(Ximag, soft_threshold)
+            F_thin = X_tempreal + 1j* X_tempimag
+            
+            #Thick structures
+            F_comb = form_P_meas(W, F_thin+F_thick, phi, lambda2, m)
+            residual = P - F_comb
+            F_thick = F_thick + form_F_li(residual, phi, lambda2, lambda2_ref, n)
+            
+            A_re, B_re, C_re, D_re = pywt.wavedec(F_thick.real, db8, level=3, mode='zpd')
+            A_im, B_im, C_im, D_im = pywt.wavedec(F_thick.imag, db8, level=3, mode='zpd')
+            
+            A_re = softThreshold(A_re, soft_threshold)
+            B_re = softThreshold(B_re, soft_threshold)
+            C_re = softThreshold(C_re, soft_threshold)
+            D_re = softThreshold(D_re, soft_threshold)
+            A_im = softThreshold(A_im, soft_threshold)
+            B_im = softThreshold(B_im, soft_threshold)
+            C_im = softThreshold(C_im, soft_threshold)
+            D_im = softThreshold(D_im, soft_threshold)
+            
+            real_Xthick = pywt.waverec([A_re, B_re, C_re, D_re], 'db8', mode='zpd') 
+            imag_Xthick = pywt.waverec([A_im, B_im, C_im, D_im], 'db8', mode='zpd')
+            
+            F_thick = real_Xthick + 1j* imag_Xthick
+    return F_thin+F_thick, F_thin, F_thick
